a1b270743bb58698e0feffe995c8b197b4328f84
Jesiel Gomes <jesiel-gomes@hotmail.com>
2021-10-06 14:49:02 -0300

    Add files via upload

---
+++ Produtor.java
@@ -0,0 +1,26 @@
+package produtorConsumidor;
+
+public class Produtor extends Thread {
+	Numero numero;
+	
+	Produtor(Numero numero) {
+		this.numero = numero;
+	}
+	
+	@Override
+	public void run() {
+			for(int k= 0; k<10; k++) {
+				numero.produzir(this.getName(), k);
+				try {
+					sleep(100);
+				} catch (InterruptedException e) {
+					// TODO Bloco de captura gerado automaticamente
+					e.printStackTrace();
+				}
+			}
+				
+	}
+	
+	
+
+}
